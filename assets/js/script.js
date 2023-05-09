027967a006a585230601d4f19368e13fdc555423
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-05-09 12:42:04 -0400

    added js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]")
+const navCloseBtn = document.querySelector("[data-menu-close-btn]")
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+
+  });
+
+}
